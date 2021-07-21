4802a8bf963bf70f11c0e04f75008f7a51044331
Jakeman582 <jakehiance@gmail.com>
2021-07-21 19:17:23 -0400

    Updated the initial, basic solution for FizzBuzz

--- FizzBuzz/main.cpp
+++ FizzBuzz/main.cpp
@@ -2,7 +2,27 @@
 
 int main()
 {
-	std::cout << "..." << std::endl;
+	int number = 100;
+
+	for(int index = 1; index <= number; index++)
+	{
+		if(index % 3 == 0 && index % 5 == 0)
+		{
+			std::cout << "FizzBuzz" << std::endl;
+		}
+		else if(index % 3 == 0)
+		{
+			std::cout << "Fizz" << std::endl;
+		}
+		else if(index % 5 == 0)
+		{
+			std::cout << "Buzz" << std::endl;
+		}
+		else
+		{
+			std::cout << index << std::endl;
+		}
+	}
 
 	return 0;
 }
